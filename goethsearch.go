0a03f73f943bfbda9ce283522f7513d95d2ae185
n0cte <ilnur.a.g@gmail.com>
2018-09-25 17:08:00 +0300

    check account balance

--- goethsearch.go
+++ goethsearch.go
@@ -1,24 +1,33 @@
 package main
 
 import (
+	"context"
 	"encoding/hex"
 	"fmt"
 	"os"
 	"regexp"
 
+	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/ethereum/go-ethereum/ethclient"
 	"github.com/urfave/cli"
 )
 
+//MultiSearch - Многопоточный поиск адресов
 func MultiSearch(ctx *cli.Context) error {
 	pattern := regexp.MustCompile(ctx.String("pattern"))
 	threads := ctx.Int("threads")
 	count := ctx.Int("count")
+	check := ctx.Bool("balance")
 
 	found := 0
-
+	rpc, err := ethclient.Dial("https://mainnet.infura.io/UoiDPLlaAoM5GmpK8aR3")
+	if err != nil {
+		panic("Connection error")
+	}
 	for i := 0; i < threads; i++ {
 		go func(num int) {
+			ctx := context.Background()
 			for count > found {
 				key, err := crypto.GenerateKey()
 
@@ -30,8 +39,16 @@ func MultiSearch(ctx *cli.Context) error {
 				privateKey := hex.EncodeToString(key.D.Bytes())
 
 				if pattern.MatchString(string(address)) {
-					fmt.Printf("[%d][%d - %d] %s => %s \n", num, found+1, count, string(address), string(privateKey))
-					found++
+					if check {
+						balance, _ := rpc.BalanceAt(ctx, common.HexToAddress(address), nil)
+						if balance.String() != "0" {
+							found++
+							fmt.Printf("[%d][%d - %d] %s => %s [%d]\n", num, found, count, string(address), string(privateKey), balance)
+						}
+					} else {
+						found++
+						fmt.Printf("[%d][%d - %d] %s => %s \n", num, found, count, string(address), string(privateKey))
+					}
 				}
 			}
 		}(i)
@@ -62,6 +79,10 @@ func main() {
 			Name:  "count, c",
 			Value: 1,
 			Usage: "Number of keys",
+		},
+		cli.BoolFlag{
+			Name:  "balance, b",
+			Usage: "Check balance",
 		},
 	}
 
